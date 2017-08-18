9225b4a49f83a2b154413ff3d692980968cddb57
ian@mobisocial.us <ian@mobisocial.us>
2017-08-18 15:50:49 -0700

    change joystick behavior to function for static positioning

--- public/vjoy.js
+++ public/vjoy.js
@@ -10,12 +10,11 @@
   Phaser.Plugin.VJoy = function (game, parent) {
     Phaser.Plugin.call(this, game, parent);
 
-    this.isInTheZone = isInsideTheZone.bind(this);
 
     this.input = this.game.input;
     this.imageGroup = [];
 
-    this.imageGroup.push(this.game.add.sprite(0, 0, 'vjoy_cap'));
+    //this.imageGroup.push(this.game.add.sprite(0, 0, 'vjoy_cap'));
     this.imageGroup.push(this.game.add.sprite(0, 0, 'vjoy_body'));
     this.imageGroup.push(this.game.add.sprite(0, 0, 'vjoy_body'));
     this.imageGroup.push(this.game.add.sprite(0, 0, 'vjoy_base'));
@@ -33,7 +32,6 @@
 
   Phaser.Plugin.VJoy.prototype.settings = {
     maxDistanceInPixels: 50,
-    singleDirection: false
   };
 
   
@@ -41,90 +39,67 @@
   Phaser.Plugin.VJoy.prototype.force = 0;
   Phaser.Plugin.VJoy.prototype.isDown = false;
 
-  Phaser.Plugin.VJoy.prototype.inputEnable = function (x1, y1, x2, y2) {
-    x1 = x1 || 0;
-    y1 = y1 || 0;
-    x2 = x2 || this.game.width;
-    y2 = y2 || this.game.height;
-    this.zone = new Phaser.Rectangle(x1, y1, x2, y2);
-    this.input.onDown.add(createCompass, this);
-    this.input.onUp.remove(removeCompass, this);
+  Phaser.Plugin.VJoy.prototype.bringToTop = function () {
+
+    this.imageGroup.forEach(function (e, i) {
+        e.bringToTop();
+    }, this);
   };
 
-  Phaser.Plugin.VJoy.prototype.inputDisable = function () {
-    this.input.onDown.remove(createCompass, this);
-    this.input.onUp.remove(removeCompass, this);
-  };
+  Phaser.Plugin.VJoy.prototype.inputEnable = function (x, y) {
+    this.center = new Phaser.Point(x, y);
 
-  var initialPoint;
-
-  var isInsideTheZone = function isInsideTheZone(pointer) {
-    return this.zone.contains(pointer.position.x, pointer.position.y);
-  };
-
-  var createCompass = function createCompass(pointer) {
-    if (this.pointer || !this.isInTheZone(pointer)) {
-      return;
-    }
-
-    this.pointer = pointer;
-    this.isDown = true;
+    this.isDown = false;
 
     this.imageGroup.forEach(function (e) {
       e.visible = true;
       e.bringToTop();
 
-      e.cameraOffset.x = pointer.x;
-      e.cameraOffset.y = pointer.y;
+      e.cameraOffset.x = x;
+      e.cameraOffset.y = y;
 
     }, this);
 
-    this.preUpdate = setDirection.bind(this);
 
-    initialPoint = this.input.activePointer.position.clone();
+    this.input.onDown.add(startTracking, this);
+    this.input.onUp.add(stopTracking, this);
   };
 
-  var removeCompass = function () {
-    this.imageGroup.forEach(function (e) {
-      e.visible = false;
-    });
+  Phaser.Plugin.VJoy.prototype.inputDisable = function () {
+    this.input.onDown.remove(startTracking, this);
+    this.input.onUp.remove(stopTracking, this);
+  };
+
+  var startTracking = function startTracking(pointer) {
+      if(this.center.distance(pointer.position) <= this.settings.maxDistanceInPixels) {
+          this.pointer = pointer;
+          this.isDown = true;
+          this.preUpdate = setDirection.bind(this);
+      }
+  }
+
+  var stopTracking = function stopTracking() {
+    this.preUpdate = empty;
     this.isDown = false;
     this.force = 0;
-
-    this.preUpdate = empty;
     this.pointer = null;
-  };
+    this.imageGroup.forEach(function (e, i) {
+        e.cameraOffset.x = this.center.x;
+        e.cameraOffset.y = this.center.y;
+    }, this);
+  }
 
   var empty = function () {
   };
 
   var setDirection = function () {
-    if (!this.isInTheZone(this.pointer)) {
-      return;
-    }
-
-    if (!this.pointer.active || this.input.activePointer.isUp) {
-      removeCompass.bind(this)();
-      return;
-    }
-
-    var d = initialPoint.distance(this.pointer.position);
+    var d = this.center.distance(this.pointer.position);
     var maxDistanceInPixels = this.settings.maxDistanceInPixels;
 
-    var deltaX = this.pointer.position.x - initialPoint.x;
-    var deltaY = this.pointer.position.y - initialPoint.y;
+    var deltaX = this.pointer.position.x - this.center.x;
+    var deltaY = this.pointer.position.y - this.center.y;
 
-    if (this.settings.singleDirection) {
-      if (Math.abs(deltaX) > Math.abs(deltaY)) {
-        deltaY = 0;
-        this.pointer.position.y = initialPoint.y;
-      } else {
-        deltaX = 0;
-        this.pointer.position.x = initialPoint.x;
-      }
-    }
-
-    var angle = initialPoint.angle(this.pointer.position);
+    var angle = this.center.angle(this.pointer.position);
     this.rotation = angle;
     this.force = (d < maxDistanceInPixels ? d: maxDistanceInPixels)/maxDistanceInPixels;
 
@@ -134,8 +109,8 @@
     }
 
     this.imageGroup.forEach(function (e, i) {
-      e.cameraOffset.x = initialPoint.x + (deltaX) * i / 3;
-      e.cameraOffset.y = initialPoint.y + (deltaY) * i / 3;
+      e.cameraOffset.x = this.center.x + (deltaX) * i / 2;
+      e.cameraOffset.y = this.center.y + (deltaY) * i / 2;
     }, this);
   };
 
